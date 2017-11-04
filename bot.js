5a1f83563f00a157525fc5a3daf787b09cf5c39d
Werloxx <33370985+Werloxx@users.noreply.github.com>
2017-11-04 12:49:30 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -5,4 +5,4 @@ bot.on('ready', () => {
     console.log("Bot Ready !");
 });
 
-bot.login('Mzc2MzI1OTQyNTU1MTgxMDU2.DN8wZg.znhqL1YXX5P8iQ8bnebbqNC-ZCQ');
+bot.login('Mzc2MzM2NjU5NDA0MjkyMDk4.DN86WA.t7v7EZeBAQPXq7Rfv5yu5Fi3khI');
