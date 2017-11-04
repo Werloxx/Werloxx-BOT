0187cc9681a98cf5cf30050b2d57ba0a4e25225c
Werloxx <33370985+Werloxx@users.noreply.github.com>
2017-11-04 12:16:22 +0100

    Create bot.js

---
+++ bot.js
@@ -0,0 +1,8 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+
+bot.on('ready', () => {
+    console.log("Bot Ready !");
+});
+
+bot.login('Mzc2MzI1OTQyNTU1MTgxMDU2.DN8wZg.znhqL1YXX5P8iQ8bnebbqNC-ZCQ');
